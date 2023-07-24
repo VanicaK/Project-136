a16979f84cbd9d5c83d6c41079f5d328e62d37c5
Vanica <88245937+VanicaK@users.noreply.github.com>
2023-07-24 07:02:14 +0100

    Create sentiment.py

---
+++ sentiment.py
@@ -0,0 +1,10 @@
+import tensorflow.keras
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentence = ["Awesome Camera. Worth the price"]
+
+$('#m_button).click(function(){
+  review=$('#m_textbox').val()
+  })
